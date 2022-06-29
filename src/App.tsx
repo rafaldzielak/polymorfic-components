d2a1104f4a4707a2a97a1b9a3bf5b0cd044e46d9
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-06-29 21:48:51 +0200

    Add custom props to component and prevent not suitable props

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,9 @@ function App() {
   return (
     <div className='App'>
       <p>Hello Vite + React!</p>
-      <Text as='h1'>H1</Text>
+      <Text as='h1' color='blue'>
+        H1
+      </Text>
       <Text as='h2'>H2</Text>
       <Text as='a' href='asd'>
         H2
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,13 +1,18 @@
 import React, { ReactNode } from "react";
 
-type PropTypes<C extends React.ElementType> = {
-  as?: C;
-  children: ReactNode;
-} & React.ComponentProps<C>;
+type Rainbow = "red" | "orage" | "yellow" | "green" | "blue" | "indigo" | "violet";
 
-const Text = <C extends React.ElementType = "span">({ as, children, ...rest }: PropTypes<C>) => {
+type TextProps<C extends React.ElementType> = {
+  as?: C;
+  color?: Rainbow;
+} & Omit<React.ComponentPropsWithoutRef<C>, "color">;
+
+type Props<C extends React.ElementType> = React.PropsWithChildren<TextProps<C>> &
+  Omit<React.ComponentPropsWithoutRef<C>, keyof TextProps<C>>;
+
+const Text = <C extends React.ElementType = "span">({ children, as, ...props }: Props<C>) => {
   const Component = as || "span";
-  return <Component {...rest}>{children}</Component>;
+  return <Component {...props}>{props.children}</Component>;
 };
 
 export default Text;
